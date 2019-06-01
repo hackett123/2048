6a4488d7d26c09f10a8b1db7c626cb4cf332ce6b
Michael Hackett <hackett1@sas.upenn.edu>
2019-06-01 17:13:03 -0400

    Changed colors to avoid the empty tile being included in the set. Changed how we load values for tiles from ranks to be a lazy instantiation which is 'cached' instead or calculated each time

--- src/core/view/gui/GuiRenderer.java
+++ src/core/view/gui/GuiRenderer.java
@@ -35,6 +35,7 @@ public class GuiRenderer implements Runnable, IGuiRenderer {
 
     //Colors are up for debatefinal
     private Color COLOR_BACKGROUND = new Color(0x252f3f);
+    private Color EMPTY_COLOR = new Color(0x808dad);
     private final Color[] RANK_COLORING = {
             new Color(0x335393),
             new Color(0x7facd6),
@@ -80,6 +81,19 @@ public class GuiRenderer implements Runnable, IGuiRenderer {
 
     }
 
+    private void setTileRanks(Rank[] ranks, List<String> values) {
+        int[][] calculatedValuesMap = new int[Rank.values().length - 1][2]; // fills with 0 -> 0 for all ranks
+
+        for (Rank rank : ranks) {
+            int ord = rank.ordinal();
+            if (calculatedValuesMap[ord][0] == 0) {
+                calculatedValuesMap[ord][0] = 1;
+                calculatedValuesMap[ord][1] = (int) Math.pow(2, ord);
+            }
+            values.add("" + calculatedValuesMap[ord][1]);
+        }
+    }
+
     @Override
     public void renderBoardState(Rank[] ranks) {
         for (Rank rank : ranks) {
@@ -90,17 +104,16 @@ public class GuiRenderer implements Runnable, IGuiRenderer {
 
         List<String> values = new ArrayList<>();
         //convert to 2d array for the powers of two.
-        for (Rank rank : ranks) {
-            int value = (int) Math.pow(2, rank.ordinal());
-            values.add("" + value);
-        }
+        setTileRanks(ranks, values);
 
         for (int i = 0; i < NUM_ROWS; i++) {
             for (int j = 0; j < NUM_COLS; j++) {
                 String value = values.get(i * NUM_ROWS + j);
                 String toShow = value.equals("1") ? "" : value;
                 mTiles[i][j].setText(toShow);
-                mTiles[i][j].setBackground(RANK_COLORING[ranks[i * NUM_ROWS + j].ordinal() % RANK_COLORING.length]);
+                int rankOrdinal = ranks[i * NUM_ROWS + j].ordinal();
+                Color color = rankOrdinal == 0 ? EMPTY_COLOR : RANK_COLORING[rankOrdinal % RANK_COLORING.length];
+                mTiles[i][j].setBackground(color);
             }
         }
     }
